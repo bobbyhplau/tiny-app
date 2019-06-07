cb0408a471019ce38cb8ce0eecbeedbb4e0e9116
Bobby <bobbyhplau@gmail.com>
2019-06-07 20:18:48 +0000

    cookie-session best practices implemented. Project is more RESTful.

--- express_server.js
+++ express_server.js
@@ -1,15 +1,23 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var cookieSession = require('cookie-session');
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1hour
+
+const cookieSession = require('cookie-session');
 app.use(cookieSession({
     name: 'session',
-    keys: ["this is a key?"],
-    maxAge: 24 * 60 * 60 * 1000
+    keys: ["The documentation said that I needed a secret, so the only thing I could come up with is that my guilty pleasure is to listen to korean pop music from the girl band TWICE!"],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        domain: 'localhost:8080',
+        path: '/',
+        expires: expiryDate
+    }
 }));
 
 const isEmailInUsers = require('./usersHelp').isEmailInUsers;
@@ -17,6 +25,9 @@ const urlsForUser = require('./usersHelp').urlsForUser;
 
 const bcrypt = require('bcrypt');
 const salt = 'ppd';
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 const generateRandomString = function() {
 
@@ -59,7 +70,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+    console.log(`TinyApp listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -139,7 +150,8 @@ app.get('/u/:shortURL', (req, res) => {
     res.redirect(urlDatabase[req.params.shortURL].longURL);
 });
 
-app.post('/urls/:shortURL/delete', (req, res) => {
+//should use delete
+app.delete('/urls/:shortURL', (req, res) => {
 
     let tempShortURL = req.params.shortURL;
     let tempUserID = req.session.user_id;
@@ -152,7 +164,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     res.redirect('/urls');
 });
 
-app.post('/urls/:shortURL', (req, res) => {
+app.put('/urls/:shortURL', (req, res) => {
 
     let tempShortURL = req.params.shortURL;
     let tempUserID = req.session.user_id;
